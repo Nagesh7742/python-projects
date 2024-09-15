0c68f3e132be8bf3bca44a59ea23c365c343454b
Nagesh7742 <157815870+Nagesh7742@users.noreply.github.com>
2024-09-15 19:06:17 +0530

    Create calculator-app.py

---
+++ calculator-app.py
@@ -0,0 +1,16 @@
+#calculator app program
+
+a = int(input('enter the a value: '))
+b = int(input('enter the b value: '))
+o = input("enter operation: ")
+if o == "+":
+    print(f'the anwer is = {a+b}')
+elif o == "-":
+    print(f'the anwer is = {a-b}')
+elif o == "*":
+    print(f'the anwer is = {a*b}')
+else:
+    print("you entered a incorrect operator")
+    print("try again")
+
+    
